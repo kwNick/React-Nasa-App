3dd4daf525c51ac55cda57b7a09f35b027135797
Nickolas Piraino <kwnp17c@gmail.com>
2024-05-22 04:03:51 -0400

    cleaning up and review

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import SideBar from "./components/SideBar"
 
 function App() {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [showModal, setShowModal] = useState<boolean | null>(false);
 
   const handleToggleModal = () => {
